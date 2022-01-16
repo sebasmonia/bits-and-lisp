bfddc69c81210aa7a4d634ef169698c305dd7fed
Sebastián Monía <smonia@outlook.com>
2022-01-16 00:04:15 -0700

    Add images, start dealing with bytes

--- bits-and-lisp.lisp
+++ bits-and-lisp.lisp
@@ -7,3 +7,31 @@
 
 (defun write-file ()
   (format nil "Write"))
+
+
+(defclass bmp-file ()
+  ((path :initarg :path :accessor path)))`
+
+;; I took this from https://github.com/EuAndreh/cl-intbytes/blob/master/src/cl-intbytes.lisp
+;; and:
+;;
+;; (with-open-file (stream "red.bmp" :direction :input :element-type '(unsigned-byte 8))
+;;        (loop for value = (read-byte stream nil)
+;;              if value
+;;                collect value into le-bytes
+;;              else
+;;                do (return le-bytes)))
+;; (octets->uint (coerce (subseq red-data 2 6) 'vector) 4)
+;; 750054 (20 bits, #xB71E6)
+;;
+;; so it seems right, now I just now to understand it :upsidedown:
+(octets->uint (coerce (subseq red-data 2 6) 'vector) 4)
+(defun octets->uint (array n-bytes &optional (start 0))
+  "Interprets `N-BYTES` of a given `ARRAY` as an unsigned integer."
+  (let ((int 0))
+    (loop
+       for byte-position from 0 below (* 8 n-bytes) by 8
+       for array-position from 0 below n-bytes
+       do (setf (ldb (byte 8 byte-position) int)
+                (aref array (+ start array-position))))
+    int))
