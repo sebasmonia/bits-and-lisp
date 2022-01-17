96450035730bab0ac19ebddddf92a76ba98608e8
Sebastián Monía <smonia@outlook.com>
2022-01-16 21:51:24 -0700

    Validate BMP header, read file size from header

--- bits-and-lisp.lisp
+++ bits-and-lisp.lisp
@@ -10,28 +10,57 @@
 
 
 (defclass bmp-file ()
-  ((path :initarg :path :accessor path)))`
+  ((path :initform "" :initarg :path :accessor bmp-path)
+   (size-bytes :initform 0 :accessor bmp-size-bytes)
+   ))
 
-;; I took this from https://github.com/EuAndreh/cl-intbytes/blob/master/src/cl-intbytes.lisp
-;; and:
-;;
-;; (with-open-file (stream "red.bmp" :direction :input :element-type '(unsigned-byte 8))
-;;        (loop for value = (read-byte stream nil)
-;;              if value
-;;                collect value into le-bytes
-;;              else
-;;                do (return le-bytes)))
-;; (octets->uint (coerce (subseq red-data 2 6) 'vector) 4)
-;; 750054 (20 bits, #xB71E6)
-;;
-;; so it seems right, now I just now to understand it :upsidedown:
-(octets->uint (coerce (subseq red-data 2 6) 'vector) 4)
-(defun octets->uint (array n-bytes &optional (start 0))
-  "Interprets `N-BYTES` of a given `ARRAY` as an unsigned integer."
-  (let ((int 0))
-    (loop
-       for byte-position from 0 below (* 8 n-bytes) by 8
-       for array-position from 0 below n-bytes
-       do (setf (ldb (byte 8 byte-position) int)
-                (aref array (+ start array-position))))
-    int))
+(defun make-bmp-file ()
+  (make-instance 'bmp-file))
+
+(defun make-bmp-file-from-path (path)
+  "Create a `bmp-file' instance from a file in PATH."
+  (let (buffer
+        (the-file (make-instance 'bmp-file :path path)))
+    (flet ((consume-buffer ()
+             "Return the `nreverse' of `buffer', and `setf' it to empty (to start pushing again)."
+             (let ((bytes-in-buffer (nreverse buffer)))
+               (setf buffer nil)
+               bytes-in-buffer)))
+      (with-open-file (stream path :direction :input :element-type '(unsigned-byte 8))
+        (loop for value = (read-byte stream nil)
+              for position from 0
+              do
+                 (push value buffer) ;; accumulate until we are at a field limit
+                 (cond ((= position 1) (validate-header (consume-buffer)))
+                       ((= position 5) (setf (bmp-size-bytes the-file)
+                                             (calculate-size (consume-buffer))))
+                       ((= position 7) (return)))))
+      )
+      the-file))
+
+(defun validate-header (header-field)
+  "Check that HEADER-FIELD has the values 66 77 (BM in ASCII)."
+  (unless (and (= (first header-field) 66)
+               (= (second header-field) 77))
+    (error "Invalid BMP file - bad header.")))
+
+(defun calculate-size (size-field)
+  (byte-seq-to-integer size-field))
+
+;; Slightly modified octets->uint from
+;; https://github.com/EuAndreh/cl-intbytes/blob/master/src/cl-intbytes.lisp
+;; `ldb' (see PCL chapter 24) gets the bits of an integer given a `byte specifier'
+;; this function creates an output integer and sets its bits based on the array of integers
+;; pased as parameter, which are all bytes
+(defun byte-seq-to-integer (bytes-seq)
+  "Interpret BYTES-SEQ as an unsigned integer."
+  (let ((bytes-list (coerce bytes-seq 'list))
+        (output-int 0))
+    (loop for byte-value in bytes-list
+          for bit-displacement from 0 by 8 ;; produces 0, 8, 16, 32, etc.
+          do
+             ;; setting the bits in `output-int' to the bits in the corresponding
+             ;; `byte-value', on each iteration `bit-displacement' goes in powers of two
+             (setf (ldb (byte 8 bit-displacement) output-int)
+                   byte-value))
+    output-int))
